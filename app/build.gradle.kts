3634106cd84870074cceaa9f105f606c973aded9
Prakrati Sharma <Prakrati.sharma@maahiway.com>
2024-12-21 14:32:02 -0500

    Fixed :Build issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.maahiway.stepupsoftball"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.maahiway.stepupsoftball"
